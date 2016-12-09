64700ce5f5b57eb65753fc2d210b466d9ee5e72a
zhengyuanqian <zhengyuanqian@lifang.com>
2016-12-09 11:40:08 +0800

    Image view优化

--- JHKit/Category/UI/UIImageView+JHKit.h
+++ JHKit/Category/UI/UIImageView+JHKit.h
@@ -9,20 +9,49 @@
 #import <UIKit/UIKit.h>
 #import "UIImageView+WebCache.h"
 
+typedef NS_ENUM(NSInteger, JHWebImageOptions) {
+    //if download error, try again
+    JHWebImageOptionsRetryFailed = 1 << 0,
+    
+    //when scrollView is scroll, delay download
+    JHWebImageOptionsLowPriority = 1 << 0,
+    
+    //cache memory only
+    JHWebImageOptionsCacheMemoryOnly = 1 << 0,
+    
+    //progressive download
+    JHWebImageOptionsProgressiveDownload = 1 << 0,
+    
+    //cache disk, NSURLCache will deal
+    JHWebImageOptionsRefreshCached = 1 << 0,
+};
+
+typedef NS_ENUM(NSInteger, JHImageCacheType) {
+    JHImageCacheTypeNone,
+    JHImageCacheTypeDisk,
+    JHImageCacheTypeMemory,
+};
+
+//download progress block
+typedef void (^JHWebImageDownloadProgressBlock) (NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL);
+
+//download completion block
+typedef void (^JHWebImageDownloadCompletionBlock)(UIImage * _Nullable image, NSError * _Nullable error, JHImageCacheType cacheType, NSURL * _Nullable imageURL);
+
 @interface UIImageView (JHWebImage)
 
 - (void)setImageWithURL:(nullable NSURL *)url
        placeholderImage:(nullable UIImage *)placeholder
-                options:(SDWebImageOptions)options;
+                options:(JHWebImageOptions)options;
 
 - (void)setImageWithURL:(nullable NSURL *)url
        placeholderImage:(nullable UIImage *)placeholder
-                options:(SDWebImageOptions)options
-              completed:(nullable SDExternalCompletionBlock)completedBlock;
+                options:(JHWebImageOptions)options
+              completed:(nullable JHWebImageDownloadCompletionBlock)completedBlock;
 
 - (void)setImageWithURL:(nullable NSURL *)url
        placeholderImage:(nullable UIImage *)placeholder
-                options:(SDWebImageOptions)options
-               progress:(nullable SDWebImageDownloaderProgressBlock)progressBlock
-              completed:(nullable SDExternalCompletionBlock)completedBlock;
+                options:(JHWebImageOptions)options
+               progress:(nullable JHWebImageDownloadProgressBlock)progressBlock
+              completed:(nullable JHWebImageDownloadCompletionBlock)completedBlock;
 @end
--- JHKit/Category/UI/UIImageView+JHKit.m
+++ JHKit/Category/UI/UIImageView+JHKit.m
@@ -13,23 +13,64 @@
 
 - (void)setImageWithURL:(nullable NSURL *)url
        placeholderImage:(nullable UIImage *)placeholder
-                options:(SDWebImageOptions)options {
-    [self sd_setImageWithURL:url placeholderImage:placeholder options:options];
+                options:(JHWebImageOptions)options {
+    [self setImageWithURL:url placeholderImage:placeholder options:options completed:nil];
 }
 
 - (void)setImageWithURL:(nullable NSURL *)url
        placeholderImage:(nullable UIImage *)placeholder
-                options:(SDWebImageOptions)options
-              completed:(nullable SDExternalCompletionBlock)completedBlock {
-    [self sd_setImageWithURL:url placeholderImage:placeholder options:options completed:completedBlock];
+                options:(JHWebImageOptions)options
+              completed:(nullable JHWebImageDownloadCompletionBlock)completedBlock {
+    [self setImageWithURL:url placeholderImage:placeholder options:options progress:nil completed:completedBlock];
 }
 
 - (void)setImageWithURL:(nullable NSURL *)url
        placeholderImage:(nullable UIImage *)placeholder
-                options:(SDWebImageOptions)options
-               progress:(nullable SDWebImageDownloaderProgressBlock)progressBlock
-              completed:(nullable SDExternalCompletionBlock)completedBlock {
-    [self sd_setImageWithURL:url placeholderImage:placeholder options:options progress:progressBlock completed:completedBlock];
+                options:(JHWebImageOptions)options
+               progress:(nullable JHWebImageDownloadProgressBlock)progressBlock
+              completed:(nullable JHWebImageDownloadCompletionBlock)completedBlock {
+    //set options
+    SDWebImageOptions sdOptions = 0;
+    if (options & JHWebImageOptionsRetryFailed) {
+        sdOptions |= SDWebImageRetryFailed;
+    }
+    else if (options & JHWebImageOptionsLowPriority) {
+        sdOptions |= SDWebImageLowPriority;
+    }
+    else if (options & JHWebImageOptionsCacheMemoryOnly) {
+        sdOptions |= SDWebImageCacheMemoryOnly;
+    }
+    else if (options & JHWebImageOptionsProgressiveDownload) {
+        sdOptions |= SDWebImageProgressiveDownload;
+    }
+    else if (options & JHWebImageOptionsRefreshCached) {
+        sdOptions |= SDWebImageRefreshCached;
+    }
+    
+    //set progress
+    SDWebImageDownloaderProgressBlock sdProgressBlock = nil;
+    if (progressBlock) {
+        sdProgressBlock = ^(NSInteger receivedSize, NSInteger expectedSize, NSURL * _Nullable targetURL) {
+            progressBlock(receivedSize, expectedSize, targetURL);
+        };
+    }
+    
+    //set completion
+    SDExternalCompletionBlock sdCompletionBlock = nil;
+    if (completedBlock) {
+        sdCompletionBlock = ^(UIImage * _Nullable image, NSError * _Nullable error, SDImageCacheType cacheType, NSURL * _Nullable imageURL) {
+            JHImageCacheType imageCacheType = JHImageCacheTypeNone;
+            if (cacheType == SDImageCacheTypeDisk) {
+                imageCacheType = JHImageCacheTypeDisk;
+            }
+            else if (cacheType == SDImageCacheTypeMemory) {
+                imageCacheType = JHImageCacheTypeMemory;
+            }
+            completedBlock(image, error, imageCacheType, imageURL);
+        };
+    }
+    
+    [self sd_setImageWithURL:url placeholderImage:placeholder options:sdOptions progress:sdProgressBlock completed:sdCompletionBlock];
 }
 
 @end
